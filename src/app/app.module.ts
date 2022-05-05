3d75d893e1ae381b63dd3e59a083264ef09f371f
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2022-05-05 21:14:01 +0200

    empty login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { QuestionComponent } from './components/question/question.component';
 import { VideoComponent } from './components/video/video.component';
 import { AchievementsComponent } from './view/achievements/achievements.component';
 import { SafePipe } from './pipes/safe.pipe';
+import { LoginComponent } from './view/login/login.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { SafePipe } from './pipes/safe.pipe';
     QuestionComponent,
     VideoComponent,
     AchievementsComponent,
-    SafePipe
+    SafePipe,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
