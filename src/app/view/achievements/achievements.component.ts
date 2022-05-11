2adbba194b9d7662fd655852e2fb98f15e50a987
flond7 <elisa.utilivenza@gmail.com>
2022-05-11 18:35:37 +0200

    dynamic tab name for railways in user achievements

--- src/app/view/achievements/achievements.component.ts
+++ src/app/view/achievements/achievements.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from './../../services/api.service';
+import { mergeAll, groupBy, mergeMap, toArray } from 'rxjs';
+
 
 @Component({
   selector: 'app-achievements',
@@ -7,9 +10,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AchievementsComponent implements OnInit {
 
-  constructor() { }
+  openTab = 0;
+  tabList = ['tab1', 'tab2', 'tab3'];
+  
+  questionList = ['tab1', 'tab2', 'tab3']
+  userResults = [];
+
+  constructor(public api: ApiService) { }
 
   ngOnInit(): void {
+    this.api.getRailwayList().subscribe((res:any) => {
+      this.tabList = res.map((el: any) => el.name);
+      console.log(res)
+    })
+
+    /* this.api.getUserResults(2).pipe(
+      //create arrays of elements groupe by railwayId
+      mergeAll(),
+      groupBy((el: any) => el['railwayId']),
+      mergeMap(group => group.pipe(toArray())),
+    ).subscribe((res:any) => {
+      this.userResults = res;
+      console.log(this.userResults)
+    }) */
   }
 
 }
