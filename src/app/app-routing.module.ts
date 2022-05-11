75c3aaa05d527ba65837f8c910ea71bb554e4ef1
elisaTramontiDiSopra <elisa.tramontidisopra@gmail.com>
2022-05-11 21:42:47 +0200

    added login to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AchievementsComponent } from './view/achievements/achievements.component';
 import { RailwayComponent } from './view/railway/railway.component';
+import { LoginComponent } from './view/login/login.component';
 
 const routes: Routes = [
   //{ path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'achievements', component: AchievementsComponent },
-  { path: 'railway', component: RailwayComponent }
+  { path: 'railway', component: RailwayComponent },
+  { path: 'login', component: LoginComponent }
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
