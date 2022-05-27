b85d7f2ea6b00986e94c1e84ffba1fa58506b277
flond7 <elisa.utilivenza@gmail.com>
2022-05-27 08:41:53 +0200

    added railway route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AchievementsComponent } from './view/achievements/achievements.component';
 import { RailwayComponent } from './view/railway/railway.component';
+import { RailwayListComponent } from './view/railway-list/railway-list.component';
 import { LoginComponent } from './view/login/login.component';
 
 const routes: Routes = [
   //{ path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'railway-list', component: RailwayListComponent },
   { path: 'achievements', component: AchievementsComponent },
   { path: 'railway', component: RailwayComponent },
   { path: 'login', component: LoginComponent }
